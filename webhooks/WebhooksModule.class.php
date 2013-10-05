7fb9dd6ddf41b5df38be57e9e38ffa9c4e3d1861
ipernet <ipernet@gmail.com>
2013-10-05 18:08:02 +0200

    Improvements Synthax formatting

--- webhooks/WebhooksModule.class.php
+++ webhooks/WebhooksModule.class.php
@@ -1,98 +1,97 @@
 <?php
 
-  /**
-   * Webhooks module definition
-   *
-   * @package activeCollab.modules.webhooks
-   * @subpackage models
-   */
-  class WebhooksModule extends AngieModule {
+/**
+ * Webhooks module definition
+ *
+ * @package activeCollab.modules.webhooks
+ * @subpackage models
+ */
+class WebhooksModule extends AngieModule
+{
+	/**
+	 * Plain module name
+	 *
+	 * @var string
+	 */
+	protected $name = 'webhooks';
 
-    /**
-     * Plain module name
-     *
-     * @var string
-     */
-    protected $name = 'webhooks';
+	/**
+	 * Module version
+	 *
+	 * @var string
+	 */
+	protected $version = '1.0.416';
 
-    /**
-     * Module version
-     *
-     * @var string
-     */
-    protected $version = '1.0';
+	// ---------------------------------------------------
+	//  Events and Routes
+	// ---------------------------------------------------
 
-    // ---------------------------------------------------
-    //  Events and Routes
-    // ---------------------------------------------------
+	/**
+	 * Define module routes
+	 */
+	function defineRoutes()
+	{
+		// Admin
+		Router::map('admin_webhooks', '/admin/tools/webhooks', array('controller' => 'webhooks_admin', 'action' => 'index'));
+	}
 
-    /**
-     * Define module routes
-     */
-    function defineRoutes() {
-      // Admin
-      Router::map('admin_webhooks', '/admin/tools/webhooks', array('controller'=>'webhooks_admin', 'action'=>'index'));
-    } // defineRoutes
+	/**
+	 * Define event handlers
+	 */
+	function defineHandlers()
+	{
+		EventsManager::listen('on_admin_panel', 'on_admin_panel');
 
-    /**
-     * Define event handlers
-     */
-    function defineHandlers() {
-      EventsManager::listen('on_admin_panel', 'on_admin_panel');
+		$handlers_path	=	ENVIRONMENT_PATH.'/custom/modules/webhooks/handlers';
+		
+		if( ! is_writable($handlers_path))
+			throw new Exception($handlers_path.' is not writable!');
+		
+		$handler_tpl_content	=	file_get_contents(ENVIRONMENT_PATH.'/custom/modules/webhooks/resources/callback_template.php');
+				
+		foreach(Webhooks::getEvents() as $eventname)
+		{
+			EventsManager::listen($eventname, 'callback_'.$eventname);
 
-      foreach(Webhooks::getEvents() as $eventname)
-      {
-      	EventsManager::listen($eventname, 'callback_' . $eventname);
+			$path	=	$handlers_path.'/callback_'.$eventname.'.php';
 
-      	// grab content
-      	$content = file_get_contents(ENVIRONMENT_PATH . '/custom/modules/webhooks/resources/callback_template.php');
+			// create handler if needed
+			if( ! is_file($path))
+				file_put_contents($path, str_replace('{$eventname}', $eventname, $handler_tpl_content));
+		}
+	}
 
-      	$path = ENVIRONMENT_PATH . '/custom/modules/webhooks/handlers/callback_' . $eventname . '.php';
+	// ---------------------------------------------------
+	//  Name
+	// ---------------------------------------------------
 
-      	// create handler if needed
-      	if(!is_file($path))
-      	{
-      		if(!is_writable(dirname($path))) throw new Exception($path . ' is not writable!');
+	/**
+	 * Get module display name
+	 *
+	 * @return string
+	 */
+	function getDisplayName()
+	{
+		return lang('Webhooks');
+	}
 
-      		// alter template
-      		$fileContent = str_replace('{$eventname}', $eventname, $content);
+	/**
+	 * Return module description
+	 *
+	 * @return string
+	 */
+	function getDescription()
+	{
+		return lang('Enables you to call an external url on an event-base');
+	}
 
-      		// store file
-      		file_put_contents($path, $fileContent);
-      	}
-
-      }
-    } // defineHandlers
-
-    // ---------------------------------------------------
-    //  Name
-    // ---------------------------------------------------
-
-    /**
-     * Get module display name
-     *
-     * @return string
-     */
-    function getDisplayName() {
-      return lang('Webhooks');
-    } // getDisplayName
-
-    /**
-     * Return module description
-     *
-     * @return string
-     */
-    function getDescription() {
-      return lang('Enables you to call an external url on an event-base');
-    } // getDescription
-
-    /**
-     * Return module uninstallation message
-     *
-     * @return string
-     */
-    function getUninstallMessage() {
-      return lang('Module will be deactivated. All data generated using it will be deleted');
-    } // getUninstallMessage
-
-  }
+	/**
+	 * Return module uninstallation message
+	 *
+	 * @return string
+	 */
+	function getUninstallMessage()
+	{
+		return lang('Module will be deactivated. All data generated using it will be deleted');
+	}
+}
